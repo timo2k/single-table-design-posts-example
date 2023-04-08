0d5cfab2eea346333552ea40ef61a81ba0c17b41
Timo Ischen <info@timo-ischen.de>
2023-04-08 11:20:37 +0200

    Initial commit

---
+++ test/single-table-posts-app-deployment.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SingleTablePostsAppDeployment from '../lib/single-table-posts-app-deployment-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/single-table-posts-app-deployment-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SingleTablePostsAppDeployment.SingleTablePostsAppDeploymentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
